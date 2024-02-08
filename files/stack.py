285ff471b3ff331078226150f5fb50e3000cb2c6
theoVrxScientist <159000451+theovrx@users.noreply.github.com>
2024-02-08 15:40:46 +0100

    Correcting typos

--- files/stack.py
+++ files/stack.py
@@ -1,17 +1,20 @@
 class Stack():
     def __init__(self):
         self.content = []
-        
+
     def stack(self, value):
         self.content.append(value)
-        
+
     def is_empty(self):
         return len(self.content) == 0
-    
+
     def peek(self):
         if not self.is_empty():
             return stack[-1]
         raise Exception("Stack is empty")
-        
+
     def unstack(self):
-        del self.content
+        if not self.is_empty():
+            del self.content[-1]
+        else:
+            raise Exception("Stack is empty")
