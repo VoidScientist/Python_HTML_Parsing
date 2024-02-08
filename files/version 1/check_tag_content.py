b0a62ad1d995764bfb074bb446e0f93be9b21481
theoVrxScientist <159000451+theovrx@users.noreply.github.com>
2024-02-08 16:07:45 +0100

    Update check_tag_content.py

--- files/version 1/check_tag_content.py
+++ files/version 1/check_tag_content.py
@@ -2,17 +2,17 @@ def check_tag_content(tag: str) -> list[str]:
     """
     FUNCTION THAT CHECKS IF TAG IS AN OPEN OR CLOSE ONE
     ALSO RETRIEVE CONTENT FOR FURTHER CHECKING
-    
+
     INPUT : TAG -> String
-    
+
     OUTPUT : 1. OPEN, CLOSE OR COMMENT
              2. CONTENT
-    
+
     """
     match list(tag):
         case ["<", "/", *content, ">"]:
             return ["CLOSE", "".join(content)]
-        case ["<", "!", "-", *content, "-", ">"]:
+        case ["<", "!", "-", "-", *content, "-", "-", ">"]:
             return ["COMMENT", "".join(content)]
         case ["<", *content, ">"]:
             return ["OPEN", "".join(content)]
